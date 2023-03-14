169ff78110ead210dd5aa360a0187894dfff3015
Aryan Sangwan <55026638+ATV3320@users.noreply.github.com>
2023-03-14 18:26:59 +0530

    example tictactoe

---
+++ non-website/src/App.js
@@ -0,0 +1,21 @@
+export default function Square() {
+  return (
+    <>
+      <div className="board-row">
+        <button className="square">1</button>
+        <button className="square">2</button>
+        <button className="square">3</button>
+      </div>
+      <div className="board-row">
+        <button className="square">1</button>
+        <button className="square">2</button>
+        <button className="square">3</button>
+      </div>
+      <div className="board-row">
+        <button className="square">1</button>
+        <button className="square">2</button>
+        <button className="square">3</button>
+      </div>
+    </>
+  );
+}
