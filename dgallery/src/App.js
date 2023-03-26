33dd6d94aae9f0c4657d8ebe11f55c57de398081
Aryan Sangwan <55026638+ATV3320@users.noreply.github.com>
2023-03-26 23:48:56 +0530

    modal for wallet

--- dgallery/src/App.js
+++ dgallery/src/App.js
@@ -1,12 +1,21 @@
-import './style/App.css';
-import Gallery from './Gallery';
+import "./style/App.css";
+import Gallery from "./Gallery";
+import { connectWallet } from "./connectWallet";
+// var Web3 = require("web3");
+
+// const { ethereum } = window;
+
+
 
 function App() {
   return (
     <>
-      <h1 style={{textAlign:'center'}}>dGallery</h1>
-      <p style={{textAlign:'center'}}>Hover over the images to unblur them :p</p>
-      <Gallery/>
+      <button onClick={connectWallet}>Click me</button>
+      <h1 style={{ textAlign: "center" }}>dGallery</h1>
+      <p style={{ textAlign: "center" }}>
+        Hover over the images to unblur them :p
+      </p>
+      <Gallery />
     </>
   );
 }
--- dgallery/src/interact.js
+++ dgallery/src/interact.js
@@ -5,4 +5,27 @@ const web3 = createAlchemyWeb3(alchemyKey);
 const contractABI = require("./contractABI.json");
 const contractAddress = "0xc7e618bc9fbb97f8ef143ada36f4b8472b7a5f48";
 
+window.addEventListener('load', async () => {
+    // New web3 provider
+    if (window.ethereum) {
+        window.web3 = new Web3(ethereum);
+        try {
+            // ask user for permission
+            await ethereum.enable();
+            // user approved permission
+        } catch (error) {
+            // user rejected permission
+            console.log('user rejected permission');
+        }
+    }
+    // Old web3 provider
+    else if (window.web3) {
+        window.web3 = new Web3(web3.currentProvider);
+        // no need to ask for permission
+    }
+    // No web3 provider
+    else {
+        console.log('No web3 provider detected');
+    }
+  });
 export const listContract = new web3.eth.Contract(contractABI, contractAddress);
