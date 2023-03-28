12f2deb2326dee351763c0bda5cad71b2309ea0e
Aryan Sangwan <55026638+ATV3320@users.noreply.github.com>
2023-03-28 12:32:52 +0530

    env cleared

--- dgallery/src/interact.js
+++ dgallery/src/interact.js
@@ -1,7 +1,3 @@
-// require('dotenv').config();
-// const alchemyKey = process.env.REACT_APP_ALCHEMY_KEY;
-// const { createAlchemyWeb3 } = require("@alch/alchemy-web3");
-// const web3 = createAlchemyWeb3(alchemyKey);
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import $ from 'jquery';
